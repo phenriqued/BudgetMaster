7f049eaed5079b62ca191077ce39ca2a93c65716
phenriqued <112258290+phenriqued@users.noreply.github.com>
2025-06-24 18:32:35 -0300

    Piloto

---
+++ src/main/java/phenriqued/BudgetMaster/BudgetMasterApplication.java
@@ -0,0 +1,13 @@
+package phenriqued.BudgetMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/phenriqued/BudgetMaster/BudgetMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package phenriqued.BudgetMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
