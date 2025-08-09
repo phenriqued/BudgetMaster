2273352a4602500c27185f49527325d69a3305c7
phenriqued <112258290+phenriqued@users.noreply.github.com>
2025-08-09 12:19:11 -0300

    implementing annotations for swagger documentation

--- src/main/java/phenriqued/BudgetMaster/Controllers/ExpenseControllers/ExpenseCategoryController.java
+++ src/main/java/phenriqued/BudgetMaster/Controllers/ExpenseControllers/ExpenseCategoryController.java
@@ -1,6 +1,9 @@
 package phenriqued.BudgetMaster.Controllers.ExpenseControllers;
 
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -9,11 +12,16 @@ import phenriqued.BudgetMaster.DTOs.Expense.RequestCreateExpenseCategoryDTO;
 import phenriqued.BudgetMaster.DTOs.Expense.RequestUpdateExpenseCategoryDTO;
 import phenriqued.BudgetMaster.DTOs.Expense.ResponseCategoryDTO;
 import phenriqued.BudgetMaster.DTOs.Expense.ResponseSpendingPriorityDTO;
+import phenriqued.BudgetMaster.Infra.Security.Config.SecurityConfiguration;
 import phenriqued.BudgetMaster.Infra.Security.User.UserDetailsImpl;
 import phenriqued.BudgetMaster.Services.ExpenseService.ExpenseCategoryService;
 
 import java.net.URI;
 import java.util.List;
+
+@Tag(name = "Categoria de Despesas Controller", description = "Este controlador é responsável por criar, listar todos, listar por ID e por prioridade de gastos, " +
+        "atualizar e deletar uma categoria de despesa do usuário")
+@SecurityRequirement(name = SecurityConfiguration.SECURITY)
 
 @RestController
 @RequestMapping("/expense/category")
@@ -24,16 +32,24 @@ public class ExpenseCategoryController {
     public ExpenseCategoryController(ExpenseCategoryService categoryService) {
         this.categoryService = categoryService;
     }
-
+    @Operation(summary = "Lista todas as categorias de despesas", description = "responsável por listar todas as categorias de depesas")
+    @ApiResponse(responseCode = "200", description = "Lista com sucesso todos as categorias de despesas")
+    @ApiResponse(responseCode = "401", description = "Não autenticado.")
     @GetMapping
     public ResponseEntity<List<ResponseCategoryDTO>> listAllCategory(@AuthenticationPrincipal UserDetailsImpl userDetails){
         return ResponseEntity.ok(categoryService.listAllCategoryByUser(userDetails));
     }
+    @Operation(summary = "Lista as prioridades de gastos", description = "responsável por listar todas as as prioridades de gastos")
+    @ApiResponse(responseCode = "200", description = "Lista com sucesso todos as as prioridades de gastos")
+    @ApiResponse(responseCode = "401", description = "Não autenticado.")
     @GetMapping("/spendingPriority")
     public ResponseEntity<List<ResponseSpendingPriorityDTO>> listSpendingPriority(){
         return ResponseEntity.ok(categoryService.listSpendingPriority());
     }
-
+    @Operation(summary = "Cria uma categoria de despesas", description = "responsável por criar um entidade de categoria de despesa.")
+    @ApiResponse(responseCode = "201", description = "renda criada com sucesso")
+    @ApiResponse(responseCode = "400", description = "Caso já exista alguma categoria de despesa com nome repetido pertencente aos usuários.")
+    @ApiResponse(responseCode = "401", description = "Não autenticado.")
     @PostMapping
     public ResponseEntity<ResponseCategoryDTO> createExpenseCategory(@RequestBody RequestCreateExpenseCategoryDTO createCategoryDTO,
                                                                      @AuthenticationPrincipal UserDetailsImpl userDetails){
@@ -41,14 +57,22 @@ public class ExpenseCategoryController {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("{id}").buildAndExpand(data.getId()).toUri();
         return ResponseEntity.created(uri).body(new ResponseCategoryDTO(data));
     }
-
+    @Operation(summary = "Atualiza uma Categoria de despesas", description = "responsável por atualizar uma categoria de despesa, " +
+            "seja somente o nome quanto somente a prioridade de gasto")
+    @ApiResponse(responseCode = "204", description = "atualização feita com sucesso.")
+    @ApiResponse(responseCode = "404", description = "Categoria não encontrada ou não pertence ao usuário.")
+    @ApiResponse(responseCode = "400", description = "Dados para atualização incorretos")
+    @ApiResponse(responseCode = "401", description = "Não autenticado.")
     @PatchMapping("/{id}")
     public ResponseEntity<Void> updateExpenseCategory(@PathVariable("id") Long id, @RequestBody RequestUpdateExpenseCategoryDTO updateCategoryDTO,
                                                       @AuthenticationPrincipal UserDetailsImpl userDetails){
         categoryService.updateCategory(id, updateCategoryDTO, userDetails);
         return ResponseEntity.noContent().build();
     }
-
+    @Operation(summary = "Deleta uma categoria", description = "responsável por deletar uma Categoria")
+    @ApiResponse(responseCode = "204", description = "deleção feita com sucesso.")
+    @ApiResponse(responseCode = "404", description = "Categoria não encontradas ou não pertence ao usuário.")
+    @ApiResponse(responseCode = "401", description = "Não autenticado.")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteExpenseCategory(@PathVariable("id") Long id, @AuthenticationPrincipal UserDetailsImpl userDetails){
         categoryService.deleteCategory(id, userDetails);
--- src/main/java/phenriqued/BudgetMaster/Controllers/ExpenseControllers/ExpenseController.java
+++ src/main/java/phenriqued/BudgetMaster/Controllers/ExpenseControllers/ExpenseController.java
@@ -1,5 +1,9 @@
 package phenriqued.BudgetMaster.Controllers.ExpenseControllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -11,10 +15,14 @@ import phenriqued.BudgetMaster.DTOs.Expense.RequestCreateExpenseDTO;
 import phenriqued.BudgetMaster.DTOs.Expense.RequestUpdateExpenseDTO;
 import phenriqued.BudgetMaster.DTOs.Expense.ResponseAllExpenseDTO;
 import phenriqued.BudgetMaster.DTOs.Expense.ResponseExpenseDTO;
+import phenriqued.BudgetMaster.Infra.Security.Config.SecurityConfiguration;
 import phenriqued.BudgetMaster.Infra.Security.User.UserDetailsImpl;
 import phenriqued.BudgetMaster.Services.ExpenseService.ExpenseService;
 
 import java.net.URI;
+
+@Tag(name = "Despesa Controller", description = "Este controlador é responsável por criar, listar todos, listar por ID e por categoria, atualizar e deletar uma despesa do usuário")
+@SecurityRequirement(name = SecurityConfiguration.SECURITY)
 
 @RestController
 @RequestMapping("/expense")
@@ -25,35 +33,55 @@ public class ExpenseController {
     public ExpenseController(ExpenseService expenseService) {
         this.expenseService = expenseService;
     }
-
+    @Operation(summary = "Lista todas as depesas", description = "responsável por listar todas as depesas e o total de montante de despesas.")
+    @ApiResponse(responseCode = "200", description = "Lista com sucesso todos as despesas")
+    @ApiResponse(responseCode = "401", description = "Não autenticado.")
     @GetMapping
     public ResponseEntity<ResponseAllExpenseDTO> listAllExpense(@PageableDefault(size = 5) Pageable pageable, @AuthenticationPrincipal UserDetailsImpl userDetails){
         return ResponseEntity.ok(expenseService.getAllExpense(pageable, userDetails));
     }
+    @Operation(summary = "Cria uma despesa", description = "responsável por criar um entidade de despesa.")
+    @ApiResponse(responseCode = "201", description = "despesa criada com sucesso")
+    @ApiResponse(responseCode = "400", description = "Caso já exista alguma despesa com descrição repetida ou montante não está correto, ou seja o o valor " +
+            "deve corresponder a um ou mais dígitos numéricos positivos seguidos de um ponto e, finalmente, até dois dígitos numéricos ")
+    @ApiResponse(responseCode = "401", description = "Não autenticado.")
     @PostMapping
     public ResponseEntity<ResponseExpenseDTO> createExpense(@RequestBody @Valid RequestCreateExpenseDTO createExpenseDTO, @AuthenticationPrincipal UserDetailsImpl userDetails){
         var data = expenseService.createExpense(createExpenseDTO, userDetails);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("{id}").buildAndExpand(data.getId()).toUri();
         return ResponseEntity.created(uri).body(new ResponseExpenseDTO(data));
     }
-
+    @Operation(summary = "Retorna uma despesa", description = "responsável por retornar uma despesa procurada por ID da mesma")
+    @ApiResponse(responseCode = "200", description = "retorna a despesa correspondente ao usuário e o ID")
+    @ApiResponse(responseCode = "404", description = "Caso não exista uma despesa corresponde ao usuário ou o ID")
+    @ApiResponse(responseCode = "401", description = "Não autenticado.")
     @GetMapping("/search")
     public ResponseEntity<ResponseExpenseDTO> listExpenseById(@RequestParam("id") Long id, @AuthenticationPrincipal UserDetailsImpl userDetails){
         return ResponseEntity.ok(expenseService.getExpenseById(id, userDetails));
     }
+    @Operation(summary = "Retorna uma lista de despesas por categoria", description = "responsável por retornar uma lista de despesas por categoria")
+    @ApiResponse(responseCode = "200", description = "retorna uma lista de despesas correspondente ao usuário e a categoria")
+    @ApiResponse(responseCode = "404", description = "Caso não exista uma despesa corresponde a categoria de busca ou pertencente ao usuário")
     @GetMapping("/search/category")
     public ResponseEntity<ResponseAllExpenseDTO> listAllExpenseByCategory(@PageableDefault(size = 5) Pageable pageable, @RequestParam("category") String category,
                                                                           @AuthenticationPrincipal UserDetailsImpl userDetails){
         return ResponseEntity.ok(expenseService.getAllExpenseByCategory(pageable, category, userDetails));
     }
-
+    @Operation(summary = "Atualiza uma despesa", description = "responsável por atualizar uma despesa, seja somente a descrição quanto somente o montante")
+    @ApiResponse(responseCode = "204", description = "atualização feita com sucesso.")
+    @ApiResponse(responseCode = "404", description = "Despesa não encontradas ou não pertence ao usuário.")
+    @ApiResponse(responseCode = "400", description = "Dados para atualização incorretos")
+    @ApiResponse(responseCode = "401", description = "Não autenticado.")
     @PatchMapping("/update/{id}")
     public ResponseEntity<Void> updateExpense(@PathVariable("id") Long id, @RequestBody RequestUpdateExpenseDTO updateExpenseDTO,
                                               @AuthenticationPrincipal UserDetailsImpl userDetails){
         expenseService.updateExpense(id, updateExpenseDTO, userDetails);
         return ResponseEntity.noContent().build();
     }
-
+    @Operation(summary = "Deleta uma renda", description = "responsável por deletar uma despesa")
+    @ApiResponse(responseCode = "204", description = "deleção feita com sucesso.")
+    @ApiResponse(responseCode = "404", description = "Despesa não encontradas ou não pertence ao usuário.")
+    @ApiResponse(responseCode = "401", description = "Não autenticado.")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteExpense(@PathVariable("id") Long id, @AuthenticationPrincipal UserDetailsImpl userDetails){
         expenseService.deleteExpense(id, userDetails);
